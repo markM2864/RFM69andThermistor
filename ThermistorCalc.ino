c40c23ed1de19a1164ef6733c2c68cd1cedd5f0a
markM2864 <89712270+markM2864@users.noreply.github.com>
2021-09-25 00:36:22 -0400

    Update 9/25

--- ThermistorCalc.ino
+++ ThermistorCalc.ino
@@ -7,7 +7,15 @@
  * @date 9/19/2021
  */
 #include <math.h>
-#include <stdio.h>
+#include <RFM69.h>
+#include <RFM69_ATC.h>
+#include <RFM69_OTA.h>
+
+#define RFM69_INT     3  // 
+#define RFM69_CS      4  //
+#define RFM69_RST     2  // pin numbers
+
+RFM69 radio;
 
 //declaring global calculating averages
 const int runs = 10;    //number of runs
@@ -22,12 +30,21 @@ float B = -10.68933374*pow(10, -4);
 float C = 5.148508572*pow(10, -6);
 
 void setup() {
+  while(!Serial){delay(1);}
   Serial.begin(9600); //start serial monitor
   pinMode(A0, INPUT); //set A0 to an input to read 
                       //the output voltage from the voltage divider
+  Serial.begin(115200);
+  pinMode(RFM69_RST, OUTPUT);
+  digitalWrite(RFM69_RST, LOW);
+  digitalWrite(RFM69_RST, HIGH);
+  delay(10);
+  digitalWrite(RFM69_RST, LOW);
+  delay(10);
 }
 
 void loop() {
+
   
   double Vout = analogRead(A0) * (5/1023.0);  //analog read gives a number between 1-1023
                                               //so output voltage is calculated by
@@ -40,14 +57,14 @@ void loop() {
   
   if (i < runs)
   {
-    Serial.println();
-    Serial.print("Recording number: ");
-    Serial.println(i);
-    Serial.print("Current output voltage Vout = "); //displays the current output voltage of the divider
-    Serial.println(Vout);
-    Serial.print("Resistance of thermistor is "); //displays the current resistance
-    Serial.print(rTHR);
-    Serial.println(" ohms");
+//    Serial.println();
+//    Serial.print("Recording number: ");
+//    Serial.println(i);
+//    Serial.print("Current output voltage Vout = "); //displays the current output voltage of the divider
+//    Serial.println(Vout);
+//    Serial.print("Resistance of thermistor is "); //displays the current resistance
+//    Serial.print(rTHR);
+//    Serial.println(" ohms");
     //displays temp
     temp = 1 / (A + (B*log(rTHR)) + (C*pow(log(rTHR), 3)));//calculate temp in K
     temp -= 273.15; //convert to celcius
@@ -55,27 +72,27 @@ void loop() {
     temp /= 5;
     temp += 32; //converted to fahrenheit, the steps were expanded to avoid rounding errors
     temps[i] = temp;
-    Serial.print("Temperature is ");
-    Serial.print(temp, 3);
-    Serial.print(" degrees fahrenheit");
-    Serial.println();
+//    Serial.print("Temperature is ");
+//    Serial.print(temp, 3);
+//    Serial.print(" degrees fahrenheit");
+//    Serial.println();
     i++;  //increase the counter
   }
   else 
   {
     //Print the temperature after ten recordings
-    Serial.println();
-    Serial.println("End of trial result");
+//    Serial.println();
+//    Serial.println("End of trial result");
     float sum = 0;  //sum up all the recordings
     for(int i = 0; i < runs; i++)
     {
       sum += temps[i];
     }
     float avgTemp = sum / runs; // take the average
-    Serial.print("Average Temperature is ");
-    Serial.print(avgTemp, 3);
-    Serial.println(" degrees fahrenheit.");
-    Serial.end();
+//    Serial.print("Average Temperature is ");
+//    Serial.print(avgTemp, 3);
+//    Serial.println(" degrees fahrenheit.");
+//    Serial.end();
   }
   delay(3000);//delays the serial monitor output by 3000 ms or 3 seconds
               //just for readability
