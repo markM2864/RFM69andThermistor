09435e989e5bf14c46e77f7f18187a7f5ac5f9d5
markM2864 <89712270+markM2864@users.noreply.github.com>
2021-10-01 00:17:43 -0400

    9/30 update

--- RFM69andThermistor-main/TherReceiver/TherReceiver.ino
+++ RFM69andThermistor-main/TherReceiver/TherReceiver.ino
@@ -7,96 +7,55 @@
  * @date 9/25/2021
  */
 #include <math.h> //for pow and log functions
-#include <RH_RF69.h> //RadioHead library for radio functions
-#include <RHGenericDriver.h>
+#include <RFM69.h>
 #include <SPI.h> 
 #include <String.h> //for string functions and converting char array to string
 
 //next three lines aren't used yet
-#define NETWORKID     100 // Must be the same for all nodes
+#define NETWORKID     0 // Must be the same for all nodes
 #define MYNODEID      1   // My node ID
 #define TONODEID      2   // Destination node ID
 
-#define RF69_FREQ   433.0   //setting frequency to 433 MHz
+#define FREQUENCY   RF69_433MHZ   //setting frequency to 433 MHz
+#define ENCRYPT       false // Set to "true" to use encryption
+#define ENCRYPTKEY    "TOPSECRETPASSWRD" // Use the same 16-byte key on all nodes
 
-#if defined (__AVR_ATmega328P__)  // UNO or Feather 328P w/wing
-  #define RFM69_INT     3  // 
-  #define RFM69_CS      4  //
-  #define RFM69_RST     2  // "A"
-  #define LED           13 //not used yet
-#endif
+// Use ACKnowledge when sending messages (or not):
+#define USEACK        false // Request ACKs or not
 
 // radio driver, this is the object which uses the radio
-RH_RF69 rf69(RFM69_CS, RFM69_INT);
+RFM69 radio;
 
 //the coefficients for the Steinhart-Hart equation
 //calculated using matrix in matlab
-double A = 9.191896699*pow(10, -3);
-double B = -10.68933374*pow(10, -4);
-double C = 51.48508572*pow(10, -7);
+float A = 9.191896699*pow(10, -3);
+float B = -10.68933374*pow(10, -4);
+float C = 51.48508572*pow(10, -7);
 
 void setup() {
   Serial.begin(9600); //start serial monitor
   pinMode(A0, INPUT); //set A0 to an input to read 
                       //the output voltage from the voltage divider
 
-  while (!Serial) 
-  { 
-    delay(1); 
-  } //wait until serial console is open, remove if not tethered to computer
-
-  pinMode(LED, OUTPUT);     //not used yet, will be used to indicate data
-                            //being received or transmitted
-  pinMode(RFM69_RST, OUTPUT); //configuring the reset pin
-  digitalWrite(RFM69_RST, LOW); //set it low initially
-
-  // manual reset, setting the RST pin high triggers reset of module
-  digitalWrite(RFM69_RST, HIGH);
-  delay(10);
-  digitalWrite(RFM69_RST, LOW);
-  delay(10);
-
-  //check to make sure the reset worked
-  if (!rf69.init()) 
+  radio.initialize(FREQUENCY, MYNODEID, NETWORKID);
+  radio.setHighPower(); //required for RFM69HCW module
+  Serial.print("Node ");
+  Serial.print(MYNODEID);
+  Serial.println(" ready");
+  if(ENCRYPT)
   {
-    Serial.println("RFM69 radio init failed");
-    while (1);
+    radio.encrypt(ENCRYPTKEY);
   }
-  else
-  {
-    Serial.println("RFM69 radio init OK!");
-  }
-  //set the frequency
-  rf69.setFrequency(RF69_FREQ);
-
-  //setting the modulation configuration
-  //Rb = 250 kbs and Fd = 250 kHz
-  //error will appear if constants from the RadioHead library
-  //do not have "RH_" in front of them
-  rf69.setFrequency(RH_RF69::GFSK_Rb250Fd250);
-  // Not using encryption
-  if (!rf69.setFrequency(RF69_FREQ)) 
-  {
-    Serial.println("setFrequency failed");
-  }
-  rf69.setTxPower(20, true);  // range from 14-20 for power, 2nd arg must be true for 69HCW
-
-  // The encryption key has to be the same as the one in the server
-  uint8_t key[] = { 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08,
-                    0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08};
-  rf69.setEncryptionKey(key);
 }
 
 void loop() {
-  
-  delay(1000); //wait for a second
   double Vout = analogRead(A0) * (5/1023.0);  //analog read gives a number between 1-1023
                                               //so output voltage is calculated by
                                               //multiplying by source voltage and
                                               //dividing by 1023.
   double rTHR = (10000 * Vout) / (5 - Vout);  //equation of resistance derived from voltage divider equation
   
-  double temp; //the temperature variable
+  float temp; //the temperature variable
 
   //use the Steinhart-Hart equation to calculate temp based on current resistance
   //value, calculated in kelvins
@@ -106,39 +65,44 @@ void loop() {
   temp /= 5;
   temp += 32; //converted to fahrenheit, the steps were expanded to avoid rounding errors
 
-  delay(1000);  
-  int itemp = temp; //convert the temp value to an int
+  delay(500);  
 
   //place to store incoming data
-  uint8_t buf[RH_RF69_MAX_MESSAGE_LEN];
-  uint8_t len = sizeof(buf);
+  static char dataBuffer[62];
+  static int dataLength = 0;
   
-  //look for a message from the transmitter radio
-  if (rf69.waitAvailableTimeout(500)) {
-    if (rf69.recv(buf, &len)) {
-      if (!len) return;
-      buf[len] = 0;
-      String newBuf = (char*)buf;
-      Serial.print("Received ");
-      Serial.println((char*)buf);
+  // RECEIVING
 
-      int recTemp = newBuf.toInt();
-      //RSSI = Receiver Signal Strength Indicator
-      //commented out to increase speed of code execution
-      //Serial.print("RSSI: ");
-      //this function returns the most recent indicator for the last message received
-      //Serial.println(rf69.lastRssi(), DEC);
-      
-      // Send a reply so that the transmitter knows the message was received
-      uint8_t data[] = "Got the temp";
-      //Serial.println(recTemp);
-      rf69.send(data, sizeof(data));
-      rf69.waitPacketSent();
-      //Serial.println("Sent a reply");
-    } 
-    else 
+  // In this section, we'll check with the RFM69HCW to see
+  // if it has received any packets:
+
+  if (radio.receiveDone()) // Got one!
+  {
+    // Print out the information:
+    
+    Serial.print("received from node ");
+    Serial.print(radio.SENDERID, DEC);
+    Serial.print(": [");
+
+    // The actual message is contained in the DATA array,
+    // and is DATALEN bytes in size:
+    
+    for (byte i = 0; i < radio.DATALEN; i++)
+      Serial.print((char)radio.DATA[i]);
+
+    // RSSI is the "Receive Signal Strength Indicator",
+    // smaller numbers mean higher power.
+    
+    Serial.print("], RSSI ");
+    Serial.println(radio.RSSI);
+
+    // Send an ACK if requested.
+    // (You don't need this code if you're not using ACKs.)
+    
+    if (radio.ACKRequested())
     {
-      Serial.println("Receive failed");
+      radio.sendACK();
+      Serial.println("ACK sent");
     }
   }
 }
